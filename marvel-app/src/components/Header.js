6f9d454148c378febe6cfda3c596be6370be1cf8
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-07-29 13:29:50 -0400

    add flex stylingstyling Header and App components

---
+++ marvel-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+function Header() {
+    return(
+        <header className="header-container">
+            <h1>Marvel Roster</h1>
+        </header>
+    )
+}
+
+export default Header
