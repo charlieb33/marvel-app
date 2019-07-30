60dc8d22d3a3235f08c5369d0e38b0532a47bd03
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-07-30 13:51:45 -0400

    move piece of code from App to List, and edit constructor in App

--- marvel-app/src/App.js
+++ marvel-app/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import List from "./components/List"
 import axios from "axios";
-import { ItemImage, ItemText } from "./components/common";
 import "./App.css";
 
 const key = process.env.REACT_APP_DATA_KEY;
@@ -11,8 +10,8 @@ const timestamp = process.env.REACT_APP_DATA_TIMESTAMP;
 
 class App extends Component {
   constructor() {
-    super();
-    this.setState = {
+    super()
+    this.state = {
       characters: []
     }
   }
@@ -20,7 +19,7 @@ class App extends Component {
   async componentDidMount() {
     await this.fetchData()
   }
-
+  
   fetchData = async () => {
     const url = `https://gateway.marvel.com:443/v1/public/characters?orderBy=-modified&limit=10&ts=${timestamp}&apikey=${key}&hash=${hash}`;
     const response = await axios.get(url);
@@ -30,40 +29,11 @@ class App extends Component {
     })
   }
 
-  renderItems = () => {
-    const { characters } = this.state
-    if (characters) {
-      return characters.map(char => {
-        const {
-          id,
-          name,
-          description,
-          thumbnail: { path, extension },
-          comics: { items }
-          } = char
-          return (
-            <div className="item-container" key={id}>
-              <ItemImage
-                src={`${path}.${extension}`}
-                alt={`character-image`}
-              />
-              <ItemText
-                name={name}
-                description={description ? description : "Description Unavailable"}
-                comic1={items[0].name}
-                comic2={items[7].name}
-              />
-            </div>
-          )
-      })
-    }
-  }
-
   render() {
     return (
       <div className="app-container">
         <Header />
-        <List handleShow={this.renderItems}/>
+        <List characters={this.state.characters}/>
       </div>
     )
   }
